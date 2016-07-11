3541d1b0d79d83378db8bd39ed9768a16aacab4f
Apophus <bkilel12@gmail.com>
2016-07-11 23:58:33 +0300

    added configurations for db URI

--- todoapp.py
+++ todoapp.py
@@ -2,7 +2,7 @@
 
 from flask import Flask
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/todoapp'
 from views import *
 
 if __name__ == '__main__':
